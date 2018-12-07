9961fb923578d25cb603c5ad3438d5d7019dfb41
Anton Tsyvyan <Anton.Tsyvyan@EUROPE.KWLCORP.NET>
2018-12-07 11:42:33 +0100

    this is commit from vs code

--- GIT_PROGECT/src/com/foursoft/git/test/Main.java
+++ GIT_PROGECT/src/com/foursoft/git/test/Main.java
@@ -3,8 +3,7 @@ package com.foursoft.git.test;
 public class Main {
 
 	public Main() {
-		
-		//TODO main flow
+	
 		System.out.println("Hello git, How are you");
 		System.out.println("I am fine, thanks");
 		//Changed by Anton
